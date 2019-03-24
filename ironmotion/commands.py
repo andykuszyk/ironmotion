3d20f719f862a1080a548ede5bea3029fc1d1d04
andykuszyk <andy.kuszyk@carfinance247.co.uk>
2019-03-24 17:11:21 +0000

    Completed first operational draft

--- ironmotion/commands.py
+++ ironmotion/commands.py
@@ -1,8 +1,10 @@
-from ironmotion.listeners import RecordListener
+from ironmotion.listeners import RecordListener, ListenListener
 from ironmotion.sdk import Leap
 import pickle
 import subprocess
 from pynput.keyboard import Key, Controller
+import json
+import distance as dist
 
 
 def record(args):
@@ -29,15 +31,38 @@ def distance(args):
         pass
     listened_translations = [(t.x, t.y, t.z) for t in listener.translations]
 
-    mse = distance.simple(recorded_translations, listened_translations)
+    mse = dist.simple(recorded_translations, listened_translations)
 
-    print 'The displacement delta is: {}'.format(displacement_delta)
     print 'The MSE is: {}'.format(mse)
+
+
+class Action:
+    def __init__(self, action_value, action_type):
+        self._value = action_value
+        self._type = action_type
+
+    def invoke(self):
+        keyboard = Controller()
+        if self._type == 'cmd':
+            subprocess.call(self._value.split(' '))
+        elif self._type == 'key':
+            if ' ' not in self._value:
+                keyboard.press(get_key(self._value))
+                keyboard.release(get_key(self._value))
+            else:
+                keys = [get_key(k) for k in self._value.split(' ')]
+                for key in keys:
+                    keyboard.press(key)
+                for key in keys.reverse():
+                    keyboard.release(key)
+        else:
+            raise Exception('Unrecognised action type: {}'.format(self._type))
 
 
 def listen(args):
     try:
-        config = json.loads(args.config_file)
+        with open(args.config_file, 'r') as config_file:
+            config = json.loads(config_file.read())
     except Exception as e:
         raise Exception('Error reading config file: {}'.format(e))
 
@@ -46,40 +71,27 @@ def listen(args):
 
     gestures = []
     for element in config:
-        if 'file' not in config or 'name' not in config or 'action' not in config:
-            raise Exception('Malformed config: missing file, name or action')
-        if 'type' not in config['action'] or 'value' not in config['action']:
+        if u'file' not in element or u'name' not in element or u'action' not in element or u'threshold' not in element:
+            raise Exception('Malformed element: missing file, name, threshold or action')
+        if u'type' not in element['action'] or u'value' not in element['action']:
             raise Exception('Malformed config: action missing type or value')
 
         gesture = {}
         gestures.append(gesture)
         try:
-            gesture['file'] = pickle.loads(config['file'])
+            with open(str(element[u'file']), 'rb') as file:
+                gesture['translations'] = pickle.load(file)
         except Exception as e:
-            raise Exception('Error loading gesture file ({}): {}'.format(config['file'], e))
+            raise Exception('Error loading gesture file ({}): {}'.format(element['file'], e))
 
-        gesture['name'] = config['name']
-        gesture['threshold'] = config['threshold']
-
-        if action['type'] == 'cmd':
-            gesture['action'] = lambda: subprocess.call(action['value'].split(' ')
-        elif action['type'] == 'key':
-            gesture['action'] = lambda: (
-                if ' ' not in action['value']:
-                    keyboard.press(get_key(action['value'])
-                    keyboard.release(get_key(action['value'])
-                else:
-                    keys = [get_key(k) for k in action['value'].split(' ')]
-                    for key in keys:
-                        keyboard.press(key)
-                    key key in keys.reverse():
-                        keyboard.release(key)
-            )
-        else:
-            raise Exception('Unrecognised action type: {}'.format(action['type'])
-
-    listener = ListenListener(gestures)
+        gesture['name'] = element[u'name']
+        gesture['threshold'] = element[u'threshold']
+        gesture['action'] = Action(element[u'action'][u'value'], element[u'action'][u'type'])
+        
+    listener = ListenListener()
+    listener.set_gestures(gestures)
     controller = Leap.Controller(listener)
+    input('')
 
 
 def get_key(key):
--- ironmotion/listeners/__init__.py
+++ ironmotion/listeners/__init__.py
@@ -1,4 +1,6 @@
 from ironmotion.sdk import Leap
+from ironmotion import distance
+
 
 class RecordListener(Leap.Listener):
     def on_init(self, controller):
@@ -25,3 +27,38 @@ class RecordListener(Leap.Listener):
         self._last_frame = frame
 
 
+class ListenListener(Leap.Listener):
+    def set_gestures(self, gestures):
+        self.gestures = gestures
+
+    def on_init(self, controller):
+        self._last_frame = None
+        self._is_listening = False
+        self._translations = []
+
+    def on_frame(self, controller):
+        frame = controller.frame()
+        if self._last_frame is None:
+            self._last_frame = frame
+            return
+
+        translation = frame.translation(self._last_frame)
+        if not self._is_listening and (translation.x != 0 or translation.y != 0 or translation.z != 0):
+            self._is_listening = True
+
+        if self._is_listening:
+            if translation.x == 0 and translation.y == 0 and translation.z == 0:
+                self._is_listening = False
+                for gesture in self.gestures:
+                    mse = distance.simple(gesture['translations'], [(t.x, t.y, t.z) for t in self._translations])
+                    if mse < gesture['threshold']:
+                        print 'Found match for gesture [{}], invoking action!'.format(gesture['name'])
+                        gesture['action'].invoke()
+                    else:
+                        print 'MSE for gesture [{}] was {}, but threshold was {}, so not invoking action'.format(
+                            mse, gesture['name'], gesture['threshold'])
+                self._translations = []
+            else:
+                self._translations.append(translation)
+
+        self._last_frame = frame
