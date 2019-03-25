669a92d76f03f15def0ca8a2c1514c68457dc1a1
andykuszyk <andy.kuszyk@carfinance247.co.uk>
2019-03-25 07:06:55 +0000

    Added additional test

--- ironmotion/tests/test_distance.py
+++ ironmotion/tests/test_distance.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from ironmotion import distance
+from math import sqrt
 
 
 class TestDistance(TestCase):
@@ -15,6 +16,13 @@ class TestDistance(TestCase):
 
         self.assertEqual(mse, 0, "Results should be zero, because A and B are the same")
 
+    def test_simple_when_AB_are_separated_should_return_error(self):
+        A = [(0, 0, 0), (1, 1, 1)]
+        B = [(1, 1, 1), (3, 3, 3)]
+
+        mse = distance.simple(A, B)
+
+        self.assertEqual(mse, sqrt(3), "The result should be the root of the sum of the differences squared.")
 
 if __name__ == "__main__":
     unittest.main()
